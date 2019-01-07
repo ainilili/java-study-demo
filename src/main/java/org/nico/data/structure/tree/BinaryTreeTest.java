17442bf7a6472b89b098050111a6b52c5f53591c
nico <ainililia@163.com>
2019-01-07 17:08:18 +0800

    Updated

--- src/main/java/org/nico/data/structure/tree/BinaryTreeTest.java
+++ src/main/java/org/nico/data/structure/tree/BinaryTreeTest.java
@@ -6,13 +6,14 @@ public class BinaryTreeTest {
 
 	public static void main(String[] args) {
 		
-		int count = 10;
+		int count = 50;
 		int root = count/2;
 		BinaryTree bt = new BinaryTree(root, root);
+		Random random = new Random();
 		
 		int loop = count;
 		while(loop -- > 0) {
-			int index = loop;
+			int index = random.nextInt(count);
 			bt.insert(index, index);
 		}
 		
@@ -34,14 +35,14 @@ public class BinaryTreeTest {
 		bt.insert(10, 10);
 		System.out.println(bt);
 		
-		System.out.println("移除结点9和3：");
-		bt.remove(9);
-		bt.remove(3);
+		System.out.println("移除结点4和10：");
+		bt.remove(4);
+		bt.remove(10);
 		System.out.println(bt);
-		
-		System.out.println("删除root:");
-		bt.remove(root);
-		System.out.println(bt);
+//		
+//		System.out.println("删除root:");
+//		bt.remove(root);
+//		System.out.println(bt);
 		
 	}
 	
