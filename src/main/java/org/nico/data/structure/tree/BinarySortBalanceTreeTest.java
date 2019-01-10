2e31a3fdd6afd03a6b0173ff3a6939616ffceb43
Nico <ainililia@163.com>
2019-01-10 20:08:23 +0800

    Added BinarySortBalanceTreeTest.java

---
+++ src/main/java/org/nico/data/structure/tree/BinarySortBalanceTreeTest.java
@@ -0,0 +1,10 @@
+package org.nico.data.structure.tree;
+
+public class BinarySortBalanceTreeTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(System.lineSeparator().getBytes().length);
+		
+	}
+}
