3704ce98efe0f201df9fa824e805990f8f7844b5
Nico <ainililia@163.com>
2019-01-18 18:37:20 +0800

    :pencil: Write the code of call back method

--- src/main/java/org/nico/middleware/binlog/BinlogDefaultCallBack.java
+++ src/main/java/org/nico/middleware/binlog/BinlogDefaultCallBack.java
@@ -1,5 +1,12 @@
 package org.nico.middleware.binlog;
 
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
 import com.github.shyiko.mysql.binlog.event.RowsQueryEventData;
 import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
@@ -9,17 +16,29 @@ public class BinlogDefaultCallBack implements BinlogCallBack{
 
     @Override
     public void onInsert(WriteRowsEventData event) {
-        System.out.println("Data：" + event.getRows());
+        System.out.println("Insert Data:" + event.getRows().stream().map(a -> { 
+            return Arrays.asList(a).stream().map(s -> {return parseValue(s);}).collect(Collectors.toList());
+        }).collect(Collectors.toList()));
     }
 
     @Override
     public void onUpdate(UpdateRowsEventData event) {
-        System.out.println("Data：" + event.getRows());
+        System.out.println("Update Data：" + event.getRows().stream().map(a -> { 
+           Map<Object, Object> map = new HashMap<>();
+           Serializable[] keys = a.getKey();
+           Serializable[] values = a.getValue();
+           for(int index = 0; index < keys.length; index ++) {
+               map.put(parseValue(keys[index]), parseValue(values[index]));
+           }
+           return map;
+        }).collect(Collectors.toList()));
     }
 
     @Override
     public void onDelete(DeleteRowsEventData event) {
-        System.out.println("Data：" + event.getRows());
+        System.out.println("Delete Data:" + event.getRows().stream().map(a -> { 
+            return Arrays.asList(a).stream().map(s -> {return parseValue(s);}).collect(Collectors.toList());
+        }).collect(Collectors.toList()));
     }
 
     @Override
@@ -27,5 +46,11 @@ public class BinlogDefaultCallBack implements BinlogCallBack{
         System.out.println("Sql：" + data.getQuery());
     }
 
-
+    public Object parseValue(Serializable o) {
+        if(o instanceof byte[]) {
+            return new String((byte[]) o);
+        }else {
+            return o;
+        }
+    }
 }
