c86196dbe821446f9948bc65043bc3c946ee8b31
Nico <ainililia@163.com>
2019-01-23 19:30:03 +0800

    MergeSort

--- src/main/java/org/nico/sort/AbstractSort.java
+++ src/main/java/org/nico/sort/AbstractSort.java
@@ -21,21 +21,32 @@ public abstract class AbstractSort {
      * 
      */
     protected int[] mergeByCache(int[] array, int ll, int lr, int rl, int rr) {
+        if(array[rl] >= array[lr]) return array;
         int[] cache = new int[rr - ll + 1];
-        int llen = lr - ll + 1;
-        int rlen = rr - rl + 1;
-        int lindex = 0;
-        int rindex = 0;
-        int cindex = 0;
-        for(int index = 0; index < llen + rlen; index ++) {
-            Integer lvalue = null;
-            Integer rvalue = null;
-            if(lindex < llen) lvalue = array[lindex + ll];
-            if(rindex < rlen) rvalue = array[rindex + rl];
-            if(null != lvalue && null != rvalue){
-                cache[cindex ++] = lvalue > rvalue ? array[rindex++ + rl] : array[lindex++ + ll];
-            }else {
-                cache[cindex ++] = lvalue == null ? array[rindex++ + rl] : array[lindex++ + ll];
+        if(array[rr] <= array[ll]) {
+            int index = 0;
+            for(int a = rl; a <= rr; a ++) {
+                cache[index ++] = array[a];
+            }
+            for(int a = ll; a <= lr; a ++) {
+                cache[index ++] = array[a];
+            }
+        }else {
+            int llen = lr - ll + 1;
+            int rlen = rr - rl + 1;
+            int lindex = 0;
+            int rindex = 0;
+            int cindex = 0;
+            for(int index = 0; index < llen + rlen; index ++) {
+                Integer lvalue = null;
+                Integer rvalue = null;
+                if(lindex < llen) lvalue = array[lindex + ll];
+                if(rindex < rlen) rvalue = array[rindex + rl];
+                if(null != lvalue && null != rvalue){
+                    cache[cindex ++] = lvalue > rvalue ? array[rindex++ + rl] : array[lindex++ + ll];
+                }else {
+                    cache[cindex ++] = lvalue == null ? array[rindex++ + rl] : array[lindex++ + ll];
+                }
             }
         }
         for(int index = 0; index < cache.length; index ++) {
--- src/test/java/org/nico/sort/test/MergeSortTest.java
+++ src/test/java/org/nico/sort/test/MergeSortTest.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.nico.sort.AbstractSort;
 import org.nico.sort.MergeSort;
+
+import com.sun.scenario.effect.Merge;
 
 public class MergeSortTest {
 
@@ -56,12 +60,16 @@ public class MergeSortTest {
     
     @Test
     public void testAccuracy() {
-        int count = 100 * 10000;
+        int count = 100 * 100000;
         int[] array = new int[count];
+        int[] array1 = new int[count];
+        int[] array2 = new int[count];
         
         int index = 0;
         while(count -- > 0) {
-            array[index ++] = count;
+            array[index ] = count;
+            array2[index ] = count;
+            array1[index ++] = count;
         }
         
         AbstractSort sort = new MergeSort();
@@ -69,16 +77,24 @@ public class MergeSortTest {
         long start = 0;
         long end = 0;
         
-//        start = System.currentTimeMillis();
-//        array = sort.sort(array);
-//        end = System.currentTimeMillis();
-//        System.out.println("nico：" + (end - start));
+        
+        start = System.currentTimeMillis();
+        Stream.of(array2).sorted().findFirst();
+        end = System.currentTimeMillis();
+        System.out.println("stream：" + (end - start));
+        
+        start = System.currentTimeMillis();
+        Arrays.sort(array);
+        end = System.currentTimeMillis();
+        System.out.println("jdk：" + (end - start));
         
         
         start = System.currentTimeMillis();
-        sort.sort(array);
+        sort.sort(array1);
         end = System.currentTimeMillis();
-        System.out.println("jdk：" + (end - start));
+        System.out.println("nico：" + (end - start));
+        
+        
         
     }
 }
