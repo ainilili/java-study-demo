ac4c91516f7f1deb827b68b53a48416c11119930
Nico <ainililia@163.com>
2019-01-23 19:38:44 +0800

    Updated

--- src/main/java/org/nico/sort/AbstractSort.java
+++ src/main/java/org/nico/sort/AbstractSort.java
@@ -16,44 +16,14 @@ public abstract class AbstractSort {
         return (start + ((diff + 1) >>> 1)) - 1;
     }
     
-    /**
-     * Merage b cache
-     * 
-     */
-    protected int[] mergeByCache(int[] array, int ll, int lr, int rl, int rr) {
-        if(array[rl] >= array[lr]) return array;
-        int[] cache = new int[rr - ll + 1];
-        if(array[rr] <= array[ll]) {
-            int index = 0;
-            for(int a = rl; a <= rr; a ++) {
-                cache[index ++] = array[a];
-            }
-            for(int a = ll; a <= lr; a ++) {
-                cache[index ++] = array[a];
-            }
-        }else {
-            int llen = lr - ll + 1;
-            int rlen = rr - rl + 1;
-            int lindex = 0;
-            int rindex = 0;
-            int cindex = 0;
-            for(int index = 0; index < llen + rlen; index ++) {
-                Integer lvalue = null;
-                Integer rvalue = null;
-                if(lindex < llen) lvalue = array[lindex + ll];
-                if(rindex < rlen) rvalue = array[rindex + rl];
-                if(null != lvalue && null != rvalue){
-                    cache[cindex ++] = lvalue > rvalue ? array[rindex++ + rl] : array[lindex++ + ll];
-                }else {
-                    cache[cindex ++] = lvalue == null ? array[rindex++ + rl] : array[lindex++ + ll];
-                }
-            }
-        }
-        for(int index = 0; index < cache.length; index ++) {
-            array[ll + index] = cache[index];
+    protected int[] swapAsc(int[] array, int first, int last) {
+        int a = array[first];
+        int b = array[last];
+        if(a > b) {
+           array[last] = a;
+           array[first] = b;
         }
         return array;
     }
-    
     
 }
--- src/main/java/org/nico/sort/MergeSort.java
+++ src/main/java/org/nico/sort/MergeSort.java
@@ -11,7 +11,7 @@ public class MergeSort extends AbstractSort{
     public int[] sort(int[] array) {
         if(array.length == 1) return array;
         if(array.length == 2) {
-            if(array[0] > array[1]) doubleAscSort(array, 0, 1);
+            if(array[0] > array[1]) swapAsc(array, 0, 1);
             return array;
         }
         return sort(array, 0, array.length - 1);
@@ -22,17 +22,46 @@ public class MergeSort extends AbstractSort{
         if(half == -1) return array;
         int lr = half;
         int rl = half + 1;
-        array = lr - start <= 1 ? doubleAscSort(array, start, lr) : sort(array, start, lr);
-        array = end - rl <= 1 ? doubleAscSort(array, rl, end) : sort(array, rl, end);
+        array = lr - start <= 1 ? swapAsc(array, start, lr) : sort(array, start, lr);
+        array = end - rl <= 1 ? swapAsc(array, rl, end) : sort(array, rl, end);
         return mergeByCache(array, start, lr, rl, end);
     }
     
-    protected int[] doubleAscSort(int[] array, int first, int last) {
-        int a = array[first];
-        int b = array[last];
-        if(a > b) {
-           array[last] = a;
-           array[first] = b;
+    /**
+     * Merage b cache
+     * 
+     */
+    protected int[] mergeByCache(int[] array, int ll, int lr, int rl, int rr) {
+        if(array[rl] >= array[lr]) return array;
+        int[] cache = new int[rr - ll + 1];
+        if(array[rr] <= array[ll]) {
+            int index = 0;
+            for(int a = rl; a <= rr; a ++) {
+                cache[index ++] = array[a];
+            }
+            for(int a = ll; a <= lr; a ++) {
+                cache[index ++] = array[a];
+            }
+        }else {
+            int llen = lr - ll + 1;
+            int rlen = rr - rl + 1;
+            int lindex = 0;
+            int rindex = 0;
+            int cindex = 0;
+            for(int index = 0; index < llen + rlen; index ++) {
+                Integer lvalue = null;
+                Integer rvalue = null;
+                if(lindex < llen) lvalue = array[lindex + ll];
+                if(rindex < rlen) rvalue = array[rindex + rl];
+                if(null != lvalue && null != rvalue){
+                    cache[cindex ++] = lvalue > rvalue ? array[rindex++ + rl] : array[lindex++ + ll];
+                }else {
+                    cache[cindex ++] = lvalue == null ? array[rindex++ + rl] : array[lindex++ + ll];
+                }
+            }
+        }
+        for(int index = 0; index < cache.length; index ++) {
+            array[ll + index] = cache[index];
         }
         return array;
     }
