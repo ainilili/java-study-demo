bd6ef6403df23c7d85122de39b4a1c4f84433418
Nico <ainililia@163.com>
2019-01-25 11:21:18 +0800

    Added Quick Breadth Sort

--- src/main/java/org/nico/sort/QuickSort.java
+++ src/main/java/org/nico/sort/QuickSort.java
@@ -1,15 +1,19 @@
 package org.nico.sort;
 
+import java.util.Stack;
+
 public class QuickSort extends AbstractSort{
 
     @Override
     public int[] sort(int[] array) {
-        quickSort(array, 0, array.length - 1);
+//        quickSortDepth(array, 0, array.length - 1);
+        quickSortBreadth(array);
         return array;
     }
 
-    public void quickSort(int[] array, int left, int right) {
+    public void quickSortDepth(int[] array, int left, int right) {
         if(left >= right) return;
+        pickMiddle(array, left, right);
         int basic = array[left];
         int l = left;
         int r = right;
@@ -19,10 +23,59 @@ public class QuickSort extends AbstractSort{
             swap(array, l, r);
         }
         swap(array, left, l);
-        quickSort(array, left, l - 1);
-        quickSort(array, l + 1, right);
+        quickSortDepth(array, left, l - 1);
+        quickSortDepth(array, l + 1, right);
     }
     
+    public void quickSortBreadth(int[] array) {
+        Stack<QuickSortPlan> stack = new Stack<QuickSort.QuickSortPlan>();
+        stack.add(new QuickSortPlan(0, array.length - 1));
+        
+        while(! stack.isEmpty()) {
+            QuickSortPlan qsp = stack.pop();
+            int left = qsp.left;
+            int right = qsp.right;
+            
+            if(left >= right) continue;
+            pickMiddle(array, left, right);
+            int basic = array[left];
+            int l = left;
+            int r = right;
+            while(l < r) {
+                while(l < r && array[r] > basic) -- r;
+                while(l < r && array[l] <= basic) ++ l;
+                swap(array, l, r);
+            }
+            swap(array, left, l);
+            
+            stack.add(new QuickSortPlan(left, l - 1));
+            stack.add(new QuickSortPlan(l + 1, right));
+        }
+    }
+    
+    
+    class QuickSortPlan{
+        int left;
+        int right;
+        public QuickSortPlan(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public void pickMiddle(int[] array, int left, int right) {
+        int leftValue = array[left];
+        int rightValue = array[right];
+        int mid = (left + ((right - left + 1) >>> 1)) - 1;
+        int midValue = array[mid];
+        int midIndex = 0;
+        if(leftValue > rightValue) {
+            midIndex = rightValue > midValue ? right : mid; 
+        }else {
+            midIndex = leftValue > midValue ? left : mid;
+        }
+        swap(array, left, midIndex);
+    }
 
 
 }
--- src/test/java/org/nico/sort/test/QuickSortTest.java
+++ src/test/java/org/nico/sort/test/QuickSortTest.java
@@ -16,12 +16,12 @@ public class QuickSortTest {
         //
         int[] array = {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
 //        int[] array = {8,5,7,3,4,1,0,100,56};
-//        int index = 0;
-//        int count = 8000;
-//        array = new int[count];
-//        while(count -- > 0) {
-//            array[index ++] = count;
-//        }
+        int index = 0;
+        int count = 80000;
+        array = new int[count];
+        while(count -- > 0) {
+            array[index ++] = count;
+        }
 
         long start = System.currentTimeMillis();
         sort.sort(array);
